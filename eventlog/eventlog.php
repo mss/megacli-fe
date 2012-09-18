ba789ba71a6dfc29ccea707f9f9d12fa6cd338e8
suleman1971@gmail.com <suleman1971@gmail.com@54dfa3e2-2df8-e06d-d1f3-7a4f46ebae1d>
2012-09-18 10:57:20 +0000

    добавлены режимы просмотра в форме просмотра евентов.

--- eventlog/eventlog.php
+++ eventlog/eventlog.php
@@ -16,29 +16,39 @@
 include ("../globalfunc.inc");
 include ("eventlog.inc");
 
-$selectedcontroller=GetIntFromRequest("selectedcontroller");//getselectedcontrollerfromrequest();
+$selectedcontroller=GetIntFromRequest("selectedcontroller");
 //echo "SELECTEDCONTROLLER=".$selectedcontroller."<br>";
+$events=GetIntFromRequest("events");
+//echo "events=".$events."<br>";
+if ($events==0) 
+    $events=64;
 
 //----- удаляем старый лог файл 
 //if ( unlink($TMPEVENTLOG) == false )
 //    echo "ERROR DELETE $TMPEVENTLOG";
 
 //----- сохраняем лог во временном каталоге
-$cmd = $MEGACLI." -AdpEventLog -GetEvents -f $TMPEVENTLOG -a".$selectedcontroller." -NoLog";
+//$cmd = $MEGACLI." -AdpEventLog -GetEvents -fatal -critical -f $TMPEVENTLOG -a".$selectedcontroller." -NoLog";
+$cmd = $MEGACLI." -AdpEventLog -GetLatest".$events." -f $TMPEVENTLOG -a".$selectedcontroller." -NoLog";
+
 exec($cmd, $outarray, $errcode);
+
 if ($errcode > 0)
     MegaCliError($outarray);
 else
     $evarray = ParseEventLog($TMPEVENTLOG); //получить массив эвентов
+
 ?>
 
 <table width="100%" border="0" cellspacing="3">
+
 <?php
 for ($i = count($evarray) - 1; $i >=0; $i--)
 {
     $evarray[$i]->Print2TR();
 }
 ?>
+
 </table>
 
 </body>
