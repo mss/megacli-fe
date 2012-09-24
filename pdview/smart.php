61ddbfebdfe4a408236cffb02780fd372d1c73aa
suleman1971@gmail.com <suleman1971@gmail.com@54dfa3e2-2df8-e06d-d1f3-7a4f46ebae1d>
2012-09-24 06:34:23 +0000

    исправлено некорректное отображение формы просмотра SMART.

--- pdview/smart.php
+++ pdview/smart.php
@@ -13,17 +13,35 @@ function main()
     //echo "SELECTEDCONTROLLER=".$selectedcontroller."<br>";
     global ${"DEVICE".$selectedcontroller};
     $cmd = $SMARTCTL . " -a -d megaraid," . $diskid . " " . ${"DEVICE".$selectedcontroller};
-    echo "".$cmd."";
+    //echo "".$cmd."<br>";
     exec($cmd, $outarray, $errcode);
-    if ($errcode > 0)
-    {
-        MegaCliError($outarray);
-	return;
-    }
+    // отключено иначе ложное сообщение об ошибке
+    // if ($errcode > 0)
+    // {
+    //     MegaCliError($outarray);
+    //	   return;
+    // }
 
-    echo "<font class=\"smallbold\">";
+    printf("<div align=\"center\">\n");
+    printf("<table width=\"95%%\" border=\"0\" cellspacing=\"0\" cellpadding=\"1\">\n");
+
+    printf("<tr bgcolor=\"#00FF00\" align=\"center\"><td>\n");
+    printf("<font color=\"#FFFFFF\"><b>\n");
+    printf("SMART VIEW (%s)\n",$cmd);
+    printf("</b></font>\n");
+    printf("</td></tr>\n");
+
+    printf("<tr bgcolor=\"#FFFFAA\"><td>\n");
+    printf("<font color=\"#000000\"><b>\n");
+    printf("SMART VIEW\n");
+    printf("<pre>");
     printstringarray($outarray);
-    echo "</font>";
+    printf("</pre>");
+    printf("</b></font>\n");
+    printf("</td></tr>\n");
+
+    printf("</table>");
+    printf("</div>");
 };
 
 ?>
